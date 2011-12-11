3937e068345e277aeda7cd1cdb48581ce2b3a725
Chris <christopher.esplin@gmail.com>
2011-12-10 17:02:15 -0700

    Deleted extraneous js files

--- js/coffee.sh
+++
@@ -1,6 +0,0 @@
-cd "$( dirname "$0" )"
-for f in *coffee
-do
-  echo "Processing $f file..."
-  coffee -c $f
-done
--- js/custom.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  $(document).ready = function() {
-    return alert("This is a hit!");
-  };
-}).call(this);
